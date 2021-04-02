e5cfe503d0b72ac3f829ff76871a997f841413b3
javaboy-github <javaboy-github@outlook.jp>
2021-04-02 17:51:03 +0900

    Revert "clean: cmakelistsを必要最低限のものに変更"
    This reverts commit 996f5f47417ab13393a653b331a9d5469b07047a.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 // 必要なものをインクルード
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+using namespace glm;
 
 int main( void )
 {
@@ -12,9 +16,9 @@ int main( void )
 		return -1;
 	}
 
-	glfwWindowHint(GLFW_SAMPLES, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 0);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+	glfwWindowHint(GLFW_SAMPLES, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
 
 
     // ウィンドウを開いて、OpenGLコンテキストを作成
@@ -28,13 +32,21 @@ int main( void )
 	}
 	glfwMakeContextCurrent(window);
 
+	// GLEWを初期化
+	if (glewInit() != GLEW_OK) {
+		fprintf(stderr, "GLEWの初期化に失敗しました\n");
+		getchar();
+		glfwTerminate();
+		return -1;
+	}
+
 	// 押されているEscキーをキャプチャできることを確認
 	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
 	// 青黒い背景を設定
-	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
 
-	while(glfwWindowShouldClose(window) == GL_FALSE) {	
+	do{
         // スクリーンをクリア。
 		glClear( GL_COLOR_BUFFER_BIT );
 
@@ -43,8 +55,13 @@ int main( void )
 		
 		// バッファーをスワップ
 		glfwSwapBuffers(window);
-		glfwWaitEvents();
+		glfwPollEvents();
+
 	} 
+    // ESCキーが押されたか、ウィンドウが閉じたかを確認
+	while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
+		   glfwWindowShouldClose(window) == 0 );
+
 	// OpenGLウィンドウを閉じ、GLFWを終了
 	glfwTerminate();
 
