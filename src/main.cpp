565af4174827e7ddb4c42c475f5f116b2bae0f02
javaboy-github <javaboy-github@outlook.jp>
2021-04-03 15:58:32 +0900

    add: createProgram関数を追加

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,44 @@
 #include <glm/glm.hpp>
 using namespace glm;
 
+// プログラムオブジェクトを作成します
+// @param vsrc バーテックスシェーダのソースプログラムの文字列
+// @param fsrc フラグメントシェーダのソースプログラムの文字列
+// @return 作成されたプログラム
+GLuint createProgram(const char *vsrc, const char *fsrc) {
+	// プログラムを作成する
+	const GLuint program(glCreateProgram());
+	
+	if (vsrc != NULL ){
+		// バーテックスシェーダのシェーダオブジェクトを作成シェーダのシェーダオブジェクトを作成するシェーダオブジェクトを作成する
+		const GLuint vobj(glCreateShader(GL_VERTEX_SHADER));
+		glShaderSource(vobj, 1, &vsrc, NULL);
+		glCompileShader(vobj);
+	
+		// バーテックスシェーダのシェーダオブジェクトをプログラムオブジェクトに組み込む
+		glAttachShader(program, vobj);
+		glDeleteShader(vobj);
+	} 
+	if (fsrc != NULL) {
+		// フラグメントシェーダのシェーダオブジェクトを作成する
+		const GLuint fobj(glCreateShader(GL_FRAGMENT_SHADER));
+		glShaderSource(fobj, 1, &fsrc, NULL);
+		glCompileShader(fobj);
+
+		// フラグメントシェーダのシェーダオブジェクトをプログラムオブジェクトに組み込む
+		glAttachShader(program, fobj);
+		glDeleteShader(fobj);
+	}
+
+	// プログラムオブジェクトをリンクする
+	glBindAttribLocation(program, 0, "position");
+	glBindFragDataLocation(program, 0, "fragment");
+	glLinkProgram(program);
+
+	// 作成したプログラムオブジェクトを返す
+	return program;
+}
+
 int main( void )
 {
 	// GLFWを初期化
@@ -46,6 +84,17 @@ int main( void )
 	// 青黒い背景を設定
 	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
 
+	// GLSLを読み込む
+	const GLuint program(glCreateProgram());
+	static const GLchar *vsrc[] = {
+		"#version 150 core\n",
+		"in vec4 position;",
+		"void main()",
+		"{",
+		"  gl_Position = position",
+		"}"
+	};
+	
 	do{
         // スクリーンをクリア。
 		glClear( GL_COLOR_BUFFER_BIT );
@@ -60,7 +109,7 @@ int main( void )
 	} 
     // ESCキーが押されたか、ウィンドウが閉じたかを確認
 	while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
-		   glfwWindowShouldClose(window) == 0 );
+		glfwWindowShouldClose(window) == 0 );
 
 	// OpenGLウィンドウを閉じ、GLFWを終了
 	glfwTerminate();
